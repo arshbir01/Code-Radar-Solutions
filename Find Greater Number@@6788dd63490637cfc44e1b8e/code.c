86a846ddd7f54f6931909a0b4390841b557232dc
Arshbir Singh <arshbirsingh66@gmail.com>
2025-02-16 20:59:00 +0530

    Solution to Find Greater Number

--- Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -1,38 +1,36 @@
-// #include<stdio.h>
-// int main()
-// {
-//     int a,b;
-//     printf("");
-//     scanf("%d %d",&a,&b);
-//     if(a>b){
-//         printf("%d",a);
-//     }
-//     else if(b>a){
-//         printf("%d",b);
-//     }
-//     else{
-//         printf("");
-//     }
-//     return 0;
-// }
-
-
-
-
 #include<stdio.h>
-
 int main()
 {
-    int a, b;
+    int a,b;
     printf("");
-    scanf("%d %d", &a, &b);  // Read two integers from the user
-    
-    if(a > b) {
-        printf("%d\n", a);  // Print a if a is greater than b
+    scanf("%d %d",&a,&b);
+    if(a>b){
+        printf("%d",a);
     }
-    else{
-        printf("%d\n", b);  // Print b if b is greater than a
+    else if{
+        printf("%d",b);
     }
-    
+
     return 0;
 }
+
+
+
+
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a, b;
+//     printf("");
+//     scanf("%d %d", &a, &b);  // Read two integers from the user
+    
+//     if(a > b) {
+//         printf("%d\n", a);  // Print a if a is greater than b
+//     }
+//     else{
+//         printf("%d\n", b);  // Print b if b is greater than a
+//     }
+    
+//     return 0;
+// }
