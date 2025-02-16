951c98dc28b414728cc269160a67c57116583be1
Arshbir Singh <arshbirsingh66@gmail.com>
2025-02-16 19:37:02 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -1,11 +1,16 @@
-#include <stdio.h>
-int main() {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    if(a>0&&b<0||a<0&&b>0){
-        printf("True");
-    }else{
-        printf("False");
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("");
+    scanf("%d %d", &a, &b);
+    
+    if(a>0&&b<0||a<0&&b>0) {
+        printf("True\n");
     }
+    else {
+        printf("False\n");
+    }
+    
     return 0;
 }
